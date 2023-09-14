329f49432a9aa26f1a10b58bfecde3c479ab3b5c
thayscasado <thayscasado@hotmail.com>
2023-09-13 20:08:40 -0700

    added app header

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
+import { Button } from '@/components/ui/button';
+import { Github } from 'lucide-react';
+import { Separator } from './components/ui/separator';
 export function App() {
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+    <div className="flex flex-col min-h-screen">
+      <div className="px-6 py-3 flex items-center justify-between border-b">
+        <h1 className="text-xl font-bold ">upload.ai</h1>
+        <div className="flex items-center gap-3">
+          <span className="text-sm text-muted-foreground">
+            Developed during NLW 💜
+          </span>
+          <Separator orientation="vertical" className="h-6" />
+          <Button variant={'outline'}>
+            <Github className="w-4 h-4 mr-2" />
+            Github
+          </Button>
+        </div>
+      </div>
+      <main className="flex-1 p-6 gap-6"></main>
+    </div>
   );
 }
